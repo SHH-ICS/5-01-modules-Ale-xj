4f60db405f2efc2c31bb479c3c98c21d872207fb
Alex <alex.johnston@stu.ocsb.ca>
2024-11-20 09:51:24 -0500

    Update Q2RandomRange.py

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -7,5 +7,4 @@ upper_bound = int(input("Enter the upper bound of the range: "))
 
 random_number = random.randint(lower_bound, upper_bound)
 
-# Display the result
 print(f"A random number between {lower_bound} and {upper_bound} is: {random_number}")
