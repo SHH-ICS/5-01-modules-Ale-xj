fc706b460b30871420c53e1db834105277819db3
Alex <alex.johnston@stu.ocsb.ca>
2024-11-20 09:51:12 -0500

    Update Q2RandomRange.py

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,2 +1,11 @@
-# Create a program that accepts 2 numbers from the user. 
-# Your program will output a random number between the range given by the user.
+import random
+
+
+lower_bound = int(input("Enter the lower bound of the range: "))
+upper_bound = int(input("Enter the upper bound of the range: "))
+
+
+random_number = random.randint(lower_bound, upper_bound)
+
+# Display the result
+print(f"A random number between {lower_bound} and {upper_bound} is: {random_number}")
